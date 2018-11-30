e47f2153cbb693a71917b9ccf6ab791e39b70564
BaptLesta <baptiste.lestavel@hei.fr>
2018-11-30 14:04:45 -0500

    Corrected some indent and add some comment.

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -4,18 +4,18 @@ const CONFIG        = require('../config/config');
 const {to}          = require('../services/util.service');
 
 module.exports = function(passport){
-    var opts = {};
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-    opts.secretOrKey = CONFIG.jwt_encryption;
+	var opts = {};
+	opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+	opts.secretOrKey = CONFIG.jwt_encryption;
 
-    passport.use(new Strategy(opts, async function(jwt_payload, done){
-        let err, user;
-        [err, user] = await to(User.findById(jwt_payload.user_id));
-        if(err) return done(err, false);
-        if(user) {
-            return done(null, user);
-        }else{
-            return done(null, false);
-        }
-    }));
+	passport.use(new Strategy(opts, async function(jwt_payload, done){
+		let err, user;
+		[err, user] = await to(User.findById(jwt_payload.user_id));
+		if(err) return done(err, false);
+		if(user) {
+			return done(null, user);
+		}else{
+			return done(null, false);
+		}
+	}));
 }

07f6553d6b052c9df2e4ac3ee583dacb9a3ccdd4
BaptLesta <baptiste.lestavel@hei.fr>
2018-12-05 17:01:54 -0500

    Rechanged structure.

---
+++ server/app/movie/movie.route.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const movieCtrl = require('../controllers/movie.controller');
+
+const router = express.Router(); // eslint-disable-line new-cap
+
+router
+  .route('/')
+  /** GET /api/movies - Get list of movies */
+  .get(movieCtrl.list)
+
+  /** POST /api/movies - Create new movie */
+  .post(movieCtrl.create);
+
+router
+  .route('/:movie_id')
+
+  /** GET /api/movies/:movieId - Get movie */
+  .get(movieCtrl.get);
+
+router
+  .route('/favorites')
+
+  /** GET /api/movies/favorites- Get list of favorites */
+  .get(movieCtrl.getFavorites)
+
+  /** GET /api/movies/favorites- Add favorite */
+  .post(movieCtrl.addFavorite)
+  
+  /** GET /api/movies/favorites- Remove favorite */
+  .delete();
+
+module.exports = router;
